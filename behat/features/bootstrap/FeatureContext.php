67ec665cfda1583ad71636ccc6765ea6497e0ebf
David Kohav <david@gizra.com>
2015-11-18 11:52:06 +0200

    Update the script.

--- behat/features/bootstrap/FeatureContext.php
+++ behat/features/bootstrap/FeatureContext.php
@@ -52,36 +52,86 @@ class FeatureContext extends MinkContext implements SnippetAcceptingContext {
   /**
    * @Given I login with user :arg1
    */
-  public function iLoginWithUser2($name)
-  {
+  public function iLoginWithUser($name) {
     $this->_login($name);
   }
-
 
   /**
    * @When I visit the student personal area page
    */
-  public function iVisitTheStudentPersonalAreaPage2() {
+  public function iVisitTheStudentPersonalAreaPage() {
     $this->getSession()->visit('https://student.eteachergroup.com/Login?ReturnUrl=%2f');
 
     $element = $this->getSession()->getPage();
+    // Add the username in the form.
+    $this->iWaitForCssElement('css', '#EmailOrUsername');
     $element->fillField('EmailOrUsername', $this->user);
-    sleep(3);
+    // Add the password in the form.
+    $this->iWaitForCssElement('css', '#Password' );
     $element->fillField('Password', $this->password);
     sleep(3);
-    $submit = $element->find('css',"#goButton input");
 
-    if (empty($submit)) {
-      throw new \Exception(sprintf("No submit button at %s", $this->getSession()->getCurrentUrl()));
-    }
-
-    $this->clear();
     // Log in.
+    $submit = $element->find('css',"#goButton input");
     $submit->click();
   }
 
-  public function clear() {
-    $this->user = "";
-    $this->password = "";
+  /**
+   * @Then /^I wait for css element "([^"]*)" to "([^"]*)"$/
+   */
+  public function iWaitForCssElement($element, $appear = 'appear') {
+    $xpath = $this->getSession()->getSelectorsHandler()->selectorToXpath('css', $element);
+    $this->waitForXpathNode($xpath, $appear == 'appear');
   }
+
+  /**
+   * Wait for an element by its XPath to appear or disappear.
+   *
+   * @param string $xpath
+   *   The XPath string.
+   * @param bool $appear
+   *   Determine if element should appear. Defaults to TRUE.
+   *
+   * @throws Exception
+   */
+  private function waitForXpathNode($xpath, $appear = TRUE) {
+    $this->waitFor(function($context) use ($xpath, $appear) {
+      try {
+        $nodes = $context->getSession()->getDriver()->find($xpath);
+        if (count($nodes) > 0) {
+          $visible = $nodes[0]->isVisible();
+          return $appear ? $visible : !$visible;
+        }
+        return !$appear;
+      }
+      catch (WebDriver\Exception $e) {
+        if ($e->getCode() == WebDriver\Exception::NO_SUCH_ELEMENT) {
+          return !$appear;
+        }
+        throw $e;
+      }
+    });
+  }
+
+  /**
+   * Helper function; Execute a function until it return TRUE or timeouts.
+   *
+   * @param $fn
+   *   A callable to invoke.
+   * @param int $timeout
+   *   The timeout period. Defaults to 10 seconds.
+   *
+   * @throws Exception
+   */
+  private function waitFor($fn, $timeout = 15000) {
+    $start = microtime(true);
+    $end = $start + $timeout / 1000.0;
+    while (microtime(true) < $end) {
+      if ($fn($this)) {
+        return;
+      }
+    }
+    throw new \Exception('waitFor timed out.');
+  }
+
 }
